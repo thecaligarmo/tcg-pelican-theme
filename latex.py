d600162a78785515b424e272f312a86713156658
Aram Dermenjian <aram.dermenjian@gmail.com>
2020-04-20 20:59:16 +0000

    Update to pelican 4.2

--- latex.py
+++ latex.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python
 
-import hashlib 
+import hashlib
 import os
+import shutil
 import inspect
 import re
-import datetime
 from PIL import Image
 
 
@@ -19,28 +19,29 @@ def ensure_dir(file_path):
     if not os.path.exists(directory):
         os.makedirs(directory)
 
+
 def get_file_contents(file):
-    f = open(file,"r")
+    f = open(file, "r")
     c = ''
     if f.mode == 'r':
         c = f.read()
     f.close()
     return c
 
-def put_file_contents(file,contents,mode="w"):
+
+def put_file_contents(file, contents, mode="w"):
     ensure_dir(file)
-    f = open(file,mode)
+    f = open(file, mode)
     f.write(contents)
     f.close()
-
 
 
 def get_latex_img(latex, class_name, mdfilename, path):
     orig = latex
 
     # Grab header content
-    latex_head_contents = '\\documentclass[preview,12pt]{standalone}\n'
-    latex_head_contents += get_file_contents(os.path.join(path,"header.conf"))
+    latex_head_contents = '\\documentclass[preview, 12pt]{standalone}\n'
+    latex_head_contents += get_file_contents(os.path.join(path, "header.conf"))
 
     # setup contents
     latex_contents = latex_head_contents + '\\begin{document}\n'
@@ -49,27 +50,39 @@ def get_latex_img(latex, class_name, mdfilename, path):
 
     # Create hash for the latex filename
     latex_file_name = mdfilename+hashlib.md5(latex_contents).hexdigest()
-    img_file_path = os.path.join(path,cache_dir+latex_file_name+".png")
-    tmp_path = os.path.join(path,"./tmp/")
+    img_file_path = os.path.join(path, cache_dir+latex_file_name+".png")
+    tmp_path = os.path.join(path, "./tmp/")
     latex_file_path = tmp_path+latex_file_name
-    src_file_path = cache_dir.replace('./content','')
+    src_file_path = cache_dir.replace('./content', '')
 
     if not os.path.exists(img_file_path):
-        put_file_contents(latex_file_path+'.tex',latex_contents)
+        put_file_contents(latex_file_path+'.tex', latex_contents)
 
-        pdf_made = os.system("pdflatex -output-directory "+tmp_path[:-1] +' '+latex_file_path+'.tex')
+        pdf_made = os.system("pdflatex -output-directory " + tmp_path[:-1]
+                             + ' '+latex_file_path+'.tex')
         if pdf_made == 0:
-            os.system("convert -density 1440 "+latex_file_path+".pdf -quality 100 -flatten -resize 20% -trim -transparent white -fuzz 10% "+latex_file_path+".png")
-            os.system("mv "+latex_file_path+".png "+img_file_path)
-            os.system("rm "+latex_file_path+"*")
+            convert_options = ""
+            convert_options += "-quality 100 "
+            convert_options += "-flatten "
+            convert_options += "-resize 20% "
+            convert_options += "-trim "
+            convert_options += "-transparent white "
+            convert_options += "-fuzz 10% "
+            os.system("convert -density 1440 " + latex_file_path + ".pdf "
+                      + convert_options + latex_file_path+".png")
+            os.system("mv " + latex_file_path+".png " + img_file_path)
+            os.system("rm " + latex_file_path + "*")
 
     if os.path.exists(img_file_path):
         # Get height:
         im = Image.open(img_file_path)
-        w,h = im.size
+        w, h = im.size
 
         # We want to keep old latex in case there is a problem
-        new_i = "<img class=\"latex " + class_name + "\" src=\"{static}"+src_file_path + latex_file_name +".png\" style=\"height:" + str(h/3) + "px\" />"
+        new_i = "<img "
+        new_i += "class=\"latex " + class_name + "\" "
+        new_i += "src=\"{static}" + src_file_path + latex_file_name + ".png\" "
+        new_i += "style=\"height:" + str(h/3) + "px\" />"
         new_i += "<!--" + orig + "-->"
         return new_i
     return ""
@@ -79,9 +92,10 @@ def get_latex_img(latex, class_name, mdfilename, path):
 fn = inspect.getframeinfo(inspect.currentframe()).filename
 path = os.path.dirname(os.path.abspath(fn))
 
-tmp_path = os.path.join(path,"./tmp/")
+tmp_path = os.path.join(path, "./tmp/")
+ensure_dir(tmp_path)
 for filename in os.listdir(tmp_path):
-    file_path = os.path.join(tmp_path,filename)
+    file_path = os.path.join(tmp_path, filename)
     try:
         if os.path.isfile(file_path) or os.path.islink(file_path):
             os.unlink(file_path)
@@ -92,21 +106,20 @@ for filename in os.listdir(tmp_path):
 
 
 # Make sure we actually have the cache dir
-ensure_dir(os.path.join(path,cache_dir))
-ld = os.path.join(path,latex_content_dir)
+ensure_dir(os.path.join(path, cache_dir))
+ld = os.path.join(path, latex_content_dir)
 for root, dirs, files in os.walk(ld):
     for file in files:
         if file.endswith(".md"):
 
             mdfilename = file
             mdfilepath = os.path.join(root, file)
-            
+
             # Grab file contents
             contents = get_file_contents(mdfilepath)
-            
-            matches1 = re.findall(r"(<latex>.*?</latex>)",contents, re.DOTALL)
-            matches2 = re.findall(r"(\$\$.*?\$\$)",contents, re.DOTALL)
 
+            matches1 = re.findall(r"(<latex>.*?</latex>)", contents, re.DOTALL)
+            matches2 = re.findall(r"(\$\$.*?\$\$)", contents, re.DOTALL)
 
             # For every latex tag do the following
             for i in matches1:
@@ -115,13 +128,18 @@ for root, dirs, files in os.walk(ld):
                 if i[7] == '$':
                     class_name = 'inline'
 
-
                 img = get_latex_img(i[7:-8], class_name,  mdfilename, path)
                 if img:
                     # replace contents
-                    contents = re.sub(re.escape(i),img.replace('\\','\\\\'),contents, 0, re.MULTILINE)
+                    contents = re.sub(
+                            re.escape(i),
+                            img.replace('\\', '\\\\'),
+                            contents,
+                            0,
+                            re.MULTILINE
+                        )
                 else:
-                    print "ERROR"
+                    print("ERROR")
 
             # For every latex tag do the following
             for i in matches2:
@@ -129,13 +147,17 @@ for root, dirs, files in os.walk(ld):
                 img = get_latex_img(i[1:-1], 'inline', mdfilename, path)
                 if img:
                     # replace contents
-                    contents = re.sub(re.escape(i),img.replace('\\','\\\\'),contents, 0, re.MULTILINE)
+                    contents = re.sub(
+                            re.escape(i),
+                            img.replace('\\', '\\\\'),
+                            contents,
+                            0,
+                            re.MULTILINE
+                        )
                 else:
-                    print "ERROR"
+                    print("ERROR")
 
-        
             # reopen the file and rewrite it with the new contents.
-            new_root = root.replace(latex_content_dir,content_dir)
+            new_root = root.replace(latex_content_dir, content_dir)
             new_mdfilepath = os.path.join(new_root, mdfilename)
-            put_file_contents(new_mdfilepath,contents)
-
+            put_file_contents(new_mdfilepath, contents)
--- pelicanconf.example.py
+++ pelicanconf.example.py
@@ -2,22 +2,21 @@
 # -*- coding: utf-8 -*- #
 from __future__ import unicode_literals
 import os
-
 from emojiextension import EmojiExtension
 
 
 ########################################
 # Settings
 ########################################
-
 DELETE_OUTPUT_DIRECTORY = True
-OUTPUT_PATH = '../public_html/dev/learning'
-SITEURL = 'http://thecaligarmo.com/dev/learning'
+SITEURL = 'http://thecaligarmo.com/dev/pelican' # ensure it matches output in Makefile
 
+THEME = 'theme/thecaligarmo'
+AUTHOR = u'thecaligarmo'
 
-PATH = 'content'
 TIMEZONE = 'America/Toronto'
 DEFAULT_LANG = u'en'
+
 MARKDOWN = {
         'extensions' : ['extra',EmojiExtension.create_from_json('./resources/emojis.json')],
         'extension_configs': {
@@ -63,14 +62,13 @@ GOOGLE_ANALYTICS = 'G-FSPS9B62P7'
 #### Tipue Search
 TIPUE_SEARCH = True
 
-#### Subcategories
-#PATH_METADATA= '(?P<subcategory_path>.*)/.*'
-
 #################
 # Styling
 #################
 FONT_CSS = '<link href="https://fonts.googleapis.com/css?family=Amiri&display=swap" rel="stylesheet">'
 FONT_FAMILY = "font-family: 'Amiri', serif;"
+
+# CUSTOM_CSS = ''
 
 #################
 # Colors
@@ -89,16 +87,11 @@ DEFINITION_COLOR = '#e5f9ff'
 EXAMPLE_COLOR = '#e5f9ff'
 EXAMPLE_BORDER_COLOR = '#75a5b3'
 
-# CUSTOM_CSS = ''
-
 #################
 # Header
 #################
-HEADER_TITLE = 'The Cali Garmo'
-HEADER_SUBTITLE = 'does Learning'
 HEADER_IMAGE = False
 HEADER_LOGO = False
-
 
 #################
 # Feed information
@@ -112,10 +105,10 @@ AUTHOR_FEED_RSS = None
 #################
 # Navigation
 #################
+MENUITEMS = (('Home', SITEURL),)
 DISPLAY_PAGES_ON_MENU = True
 DISPLAY_CATEGORIES_ON_MENU = True
 DEFAULT_PAGINATION = False
-
 
 CATEGORY_SAVE_AS = '{slug}.html'
 CATEGORY_URL = '{slug}.html'
@@ -130,34 +123,13 @@ ARTICLE_URL = '{category}/{slug}.html'
 ARTICLE_LANG_SAVE_AS = os.path.join('{category}', '{slug}.html')
 ARTICLE_LANG_URL = '{category}/{slug}.html'
 
-
-
-
-
-########################################
-# Text - Can alter for languages
-########################################
-AUTHOR = u'thecaligarmo'
-SITENAME = u'The Cali Garmo is Learning'
-SITE_DESCRIPTION = u'The Cali Garmo is Learning. Be it Maths, Languages, or Life Lessons.'
-THEME = 'theme/thecaligarmo'
-
-#################
-# Navigation
-#################
-MENUITEMS = (('Home', SITEURL),)
-
 #################
 # Footer
 #################
-FOOTER_LEFT_COLUMN_TITLE = 'The Cali Garmo'
 FOOTER_LEFT_MENU = (('Home','http://thecaligarmo.com/'),)
-FOOTER_MIDDLE_COLUMN_TITLE = 'Social'
 FOOTER_MIDDLE_MENU = (('IG','http://instagram.com'),)
-# FOOTER_RIGHT_COLUMN_TITLE = 'Other'
 # FOOTER_RIGHT_MENU = ()
 COPYRIGHT = '&copy; 2020'
-
 
 #################
 # Pagination
@@ -165,75 +137,10 @@ COPYRIGHT = '&copy; 2020'
 #PAGINATION_PREVIOUS = ''
 #PAGINATION_NEXT = ''
 
-#################
-# Page specific
-#################
-ARCHIVES_HEAD_TITLE = 'Archives'
-ARCHIVES_HEAD_DESCRIPTION = 'Archives'
-ARCHIVES_SUBHEADER_IMAGE = False
-ARCHIVES_SUBHEADER_TITLE = 'Archives'
-ARCHIVES_SUBHEADER_SUBTITLE = 'Where the past resides'
-ARCHIVES_CONTENT_TITLE = 'Content'
-
-ARTICLE_SUBHEADER_BY = 'By'
-ARTICLE_SUBHEADER_PUBLISHED = 'Published on'
-ARTICLE_SUBHEADER_MODIFIED = 'Last modified'
-ARTICLE_SUBHEADER_IN = 'Category:'
-ARTICLE_RELATED_POSTS_TITLE = 'Related Posts'
-COMMENT_TITLE = 'Comments'
-
-AUTHOR_HEAD_TITLE = 'Author'
-AUTHOR_HEAD_DESCRIPTION = 'Author'
-AUTHOR_SUBHEADER_SUBTITLE = 'Author'
-AUTHOR_CONTENT_TITLE = 'Articles by '
-
-AUTHORS_HEAD_TITLE = 'Authors'
-AUTHORS_HEAD_DESCRIPTION = 'Authors'
-AUTHORS_SUBHEADER_TITLE = 'Authors'
-AUTHORS_SUBHEADER_SUBTITLE = ''
-
-CATEGORIES_HEAD_TITLE = 'Categories'
-CATEGORIES_HEAD_DESCRIPTION = 'Categories'
-CATEGORIES_SUBHEADER_TITLE = 'Categories'
-CATEGORIES_SUBHEADER_SUBTITLE = ''
-
-CATEGORY_HEAD_TITLE = 'Category'
-CATEGORY_SUBHEADER_TITLE = 'Category'
-CATEGORY_SUBHEADER_SUBTITLE = ''
-CATEGORY_ARTICLES_TITLE = 'Articles'
-CATEGORY_SUBCATEGORIES_TITLE = 'Subcategories'
-
-INDEX_HEAD_TITLE = 'Home'
-INDEX_SUBHEADER_TITLE = 'The Cali Garmo'
-INDEX_SUBHEADER_SUBTITLE = ''
-INDEX_CONTENT_TITLE = 'Articles'
-
-PERIOD_ARCHIVES_HEAD_TITLE = 'Period Archives'
-PERIOD_ARCHIVES_HEAD_DESCRIPTION = 'Period Archives'
-PERIOD_ARCHIVES_SUBHEADER_TITLE = 'Period Archives'
-PERIOD_ARCHIVES_SUBHEADER_SUBTITLE = ''
-PERIOD_ARCHIVES_CONTENT_TITLE = 'Content'
-
-SEARCH_HEAD_TITLE = 'Search'
-SEARCH_SUBHEADER_TITLE = 'Search'
-SEARCH_SUBHEADER_SUBTITLE = ''
-
-TAG_HEAD_TITLE = 'Tag'
-TAG_HEAD_DESCRIPTION = 'Tag'
-TAG_SUBHEADER_TITLE = 'Tag'
-TAG_SUBHEADER_SUBTITLE = ''
-TAG_CONTENT_TITLE = 'Articles tagged with'
-
-TAGS_HEAD_TITLE = 'Tags'
-TAGS_HEAD_DESCRIPTION = 'Tags'
-TAGS_SUBHEADER_TITLE = 'Tags'
-TAGS_SUBHEADER_SUBTITLE = ''
-TAGS_CONTENT_TITLE = 'Tag'
-TAGS_CONTENT_LIST = ''
-
-
 import sys
 sys.path.append('.')
+from textconf import *
+
 from pelicanconf_fr import SUBSITE_FR
 
 I18N_SUBSITES = {
